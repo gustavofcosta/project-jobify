a01e04e9118608f9ae2bcf40896e29b3cd72f362
Gustavo F Costa <gufercosta@gmail.com>
2022-08-04 15:04:46 -0300

    config. node and express

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
